7a2efa9773010f6857da835f3f483226515dbd3d
Chris <hainesy91@gmail.com>
2016-03-30 21:47:35 +0100

    added examples using equality operator for matching requirements and the strcasecmp function

--- Chapter_2/02-validatingStrings.php
+++ Chapter_2/02-validatingStrings.php
@@ -20,3 +20,20 @@ if(strlen(trim($_POST['zipcode'])) != 5) {
 }
 ?>
 
+<!-- To compare two strings, use the equality operator (==) -->
+
+<?php
+if($_POST['email'] == 'president@whitehouse.gov') {
+  print "Welome, Mr President.";
+}
+?>
+
+<!--The above code will only execute if the submitted form parameter email is the all-lowercase president@whitehouse.gov. When you compare strings with = =, case is important. 
+
+To compare strings without paying attention to case, use strcasecmp(). It compares two strings while ignoring differences in capitalization. -->
+
+<?php
+if (strcasecmp($_POST['email'], 'president@whitehouse.gov') == 0) {
+  print "Welcome back, Mr President.";
+}
+?>
