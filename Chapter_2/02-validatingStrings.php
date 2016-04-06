147d453b063b4acde89187180db5b7c7d0ffd632
Christopher Haines <christopherhaines@Christophers-MacBook-Pro.local>
2016-04-06 14:47:03 +0100

    CCT-19 - created a html form to apply the php to

--- Chapter_2/02-validatingStrings.php
+++ Chapter_2/02-validatingStrings.php
@@ -1,14 +1,17 @@
-<!-- $_POST['zipcode'] holds the value of the submitted form parameter "zipcode". -->
+
 
 <?php
-$zipcode = trim($_POST['zipcode']);
+$zipcode = trim($_POST["zipcode"]);
 //Now $zipcode holds that value, with any leading or trailing spaces removed
 $zip_length = strlen($zipcode);
 //Will complain if the ZIP code is not 5 characters long
 if ($zip_length != 5) {
   print "Please enter a ZIP code that is 5 characters long.";
 }
+
 ?>
+
+<!-- $_POST['zipcode'] holds the value of the submitted form parameter "zipcode". -->
 
 <!--Using trim() protects against someone who types a ZIP code of 732 followed by two spaces. It's used to throw away unintentional or unecessary spaces and making sure you get the string length you care about. -->
 
